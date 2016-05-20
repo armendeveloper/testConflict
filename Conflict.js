2faeda400b8e95a131b1a4d7981946827ec69da6
Armen Nalbandyan <armennalbandyan22@gmail.com>
2016-05-20 20:36:55 +0400

    added some files

---
+++ Conflict.js
@@ -0,0 +1,11 @@
+(function(){
+	function test1(){
+		console.log("Hello");
+	}
+	function test2(){
+		console.log("Hesdfasdfllo");
+	}
+	function test3(){
+		console.log("asf");
+	}
+})();
---
+++ LogFunction.js
@@ -0,0 +1,6 @@
+var logFunction = function (message){
+	console.log("Barev Armen");
+	console.log(message);
+};
+logFunction("Hello World");
+dsfsd fszd
