99bd17df8e83080f8609e098819d0d48029ae78a
Armen Nalbandyan <armennalbandyan22@gmail.com>
2016-05-20 20:45:41 +0400

    test commit

--- Conflict.js
+++ Conflict.js
@@ -1,11 +1,9 @@
 (function(){
-	function test1(){
-		console.log("Hello");
-	}
 	function test2(){
 		console.log("Hesdfasdfllo");
 	}
-	function test3(){
-		console.log("asf");
+	function test1(){
+		console.log("Hello");
 	}
+	
 })();
